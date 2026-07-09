24e105f304f3413bec7a77e78399ed804ac18d99
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-10 01:50:56 +0700

    Time: 15 ms (72.60%) | Memory: 14.1 MB (15.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        before = [let.lower() for let in s if let.isalnum() == True]
         lst = [let.lower() for let in s if let.isalnum() == True]
 
-        left = 0
-        right = len(lst)-1
-        while left < right:
-            lst[left], lst[right] = lst[right], lst[left]
-            left+=1
-            right-=1
-        return lst == before
+        s = ''.join(lst)
+        palindrome = s[::-1]
+
+        if s == palindrome:
+            return True
+        else:
+            return False
