c59daad3a1f510f438a824f41c6fc5e25525abfd
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-10 19:21:23 +0700

    Time: 11 ms (50.74%) | Memory: 13.7 MB (38.92%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -8,11 +8,11 @@ class Solution(object):
         count = 0
         candidate = 0
 
-        for i in range(len(nums)):
+        for num in nums:
             if count == 0:
-                candidate = nums[i]
+                candidate = num
 
-            if nums[i] == candidate:
+            if num == candidate:
                 count+=1
             else:
                 count-=1
