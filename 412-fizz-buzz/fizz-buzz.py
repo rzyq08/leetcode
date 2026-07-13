6558a9920e3719e4ce52d2062264f50c903f73c8
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-13 17:49:23 +0700

    Time: 0 ms (100.00%) | Memory: 13.3 MB (42.02%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,17 +1,6 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        res = []
-        for i in range(1, n+1):
-            if i % 15 == 0:
-                res.append("FizzBuzz")
-            elif i % 3 == 0:
-                res.append("Fizz")
-            elif i % 5 == 0:
-                res.append("Buzz")
-            else:
-                res.append(str(i))
-        return res
+        return [
+            "FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
+            for i in range(1, n + 1)
+        ]
