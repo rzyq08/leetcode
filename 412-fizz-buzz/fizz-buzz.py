afee656f4fc2332871ff7747b94d194971427845
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-14 11:01:38 +0700

    Time: 1 ms (60.01%) | Memory: 13.4 MB (42.07%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -4,5 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        
         return ["FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i) for i in range(1, n+1)]
