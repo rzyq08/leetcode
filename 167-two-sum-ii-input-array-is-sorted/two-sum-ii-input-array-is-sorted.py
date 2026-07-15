9b966a82143711f28908bbb55f9be5d81d127591
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-15 11:35:26 +0700

    Time: 1 ms (91.63%) | Memory: 12.8 MB (82.92%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -6,8 +6,9 @@ class Solution(object):
         :rtype: List[int]
         """
         l = 0
-        r = len(numbers) - 1
-        while l<r:
+        r = len(numbers)-1
+
+        while l < r:
             sm = numbers[l] + numbers[r]
             if sm == target:
                 return [l+1, r+1]
