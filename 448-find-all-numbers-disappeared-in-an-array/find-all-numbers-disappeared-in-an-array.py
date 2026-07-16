e9d122cbe743c13ce4b60e7c4bc01f745b7d07ca
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-17 02:15:00 +0700

    Time: 25 ms (93.67%) | Memory: 26.8 MB (48.58%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -4,12 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        hashm = {}
-        for num in nums:
-            hashm[num] = hashm.get(num, 0) + 1
-        
-        res = []
-        for i in range(1, len(nums)+1):
-            if i not in hashm:
-                res.append(i)
-        return res
+        hashs = set(nums)
+        return [i for i in range(1, len(nums)+1) if i not in hashs]
