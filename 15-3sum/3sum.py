4fffbc778be0a777b29031920f4cd9cdb77eb310
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-18 12:07:41 +0700

    Time: 659 ms (84.88%) | Memory: 18.3 MB (56.15%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+
+        res = []
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i-1]:
+                continue
+            
+            l = i+1
+            r = len(nums)-1
+            while l < r:
+                sm = num + nums[l] + nums[r]
+                if sm > 0:
+                    r-=1
+                elif sm < 0:
+                    l+=1
+                else:
+                    res.append([num, nums[l], nums[r]])
+                    l+=1
+                    while l < r and nums[l] == nums[l-1]:
+                        l+=1
+        return res
