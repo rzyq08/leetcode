d5474af9f1810cbfd38a5e92e24de08fe669128b
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-20 07:56:46 +0700

    Time: 599 ms (91.01%) | Memory: 18.1 MB (94.84%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-
         res = []
         for i, num in enumerate(nums):
-            if i > 0 and num == nums[i-1]:
+            if i>0 and num == nums[i-1]:
                 continue
-            
             l = i+1
             r = len(nums)-1
+            
             while l < r:
                 sm = num + nums[l] + nums[r]
                 if sm > 0:
@@ -22,6 +21,6 @@ class Solution(object):
                 else:
                     res.append([num, nums[l], nums[r]])
                     l+=1
-                    while l < r and nums[l] == nums[l-1]:
+                    while l<r and nums[l] == nums[l-1]:
                         l+=1
         return res
