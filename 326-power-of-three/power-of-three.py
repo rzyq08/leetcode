46620b7f2e291808b8cf0272d63712f65c0b8b3f
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-22 13:12:48 +0700

    Time: 4 ms (93.25%) | Memory: 12.4 MB (17.84%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -5,8 +5,4 @@ class Solution(object):
         :rtype: bool
         """
 
-        if n <= 0:
-            return False
-        while n % 3 == 0:
-            n//=3
-        return n == 1
+        return n > 0 and 1162261467 % n == 0
