1b6b894d5d5328a2948140fc29181e43aa597925
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-07-28 07:29:40 +0700

    Time: 31 ms (64.22%) | Memory: 20.2 MB (72.73%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,16 +5,15 @@ class Solution(object):
         :rtype: List[int]
         """
         length = len(nums)
-        prefix = [1]*(length+1)
-        postfix = [1]*(length+1)
-
-        prefix[1] = nums[0]
-        for i in range(1, length):
-            prefix[i+1] = prefix[i]*nums[i]
-        for i in range(length-1, -1, -1):
-            postfix[i] = nums[i]*postfix[i+1]
-        
         res = [1]*length
-        for i in range(1, length+1):
-            res[i-1] = prefix[i-1]*postfix[i]
+
+        prefix = 1
+        for i in range(length):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(length-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
