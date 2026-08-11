17daf8621977c62894452d37ea27dcd8da80e0e8
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-08-11 14:44:12 +0700

    Time: 0 ms (100.00%) | Memory: 12.8 MB (95.26%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen={}
+        for i,num in enumerate(nums):
+            if target-num in seen:
+                return [seen[target-num], i]
+            seen[num] = i
