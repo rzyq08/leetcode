69be66bf753cf155f5e99d25985c57d8dee61a14
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-08-26 18:05:53 +0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (52.89%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = 0
+        r = len(nums)-1
+        while l<r:
+            m = l + (r-l)/2
+            if nums[m]>nums[m+1]:
+                r = m
+            else:
+                l = m+1
+        return l
